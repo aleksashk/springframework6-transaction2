05e114b3d9ba7d6d1951d127c09e529bace7ee8c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-27 16:29:55 +0300

    @Transactional(propagation)

---
+++ src/main/java/by/flameksandr/repositories/ProductRepository.java
@@ -0,0 +1,21 @@
+package by.flameksandr.repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(propagation = Propagation.REQUIRED)
+public class ProductRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Transactional(propagation = Propagation.MANDATORY)
+    public void addProduct(String name) {
+        String sql = "insert into product values(null, ?)";
+        jdbcTemplate.update(sql, name);
+    }
+}
---
+++ src/main/java/by/flameksandr/services/ProjectService.java
@@ -0,0 +1,30 @@
+package by.flameksandr.services;
+
+import by.flameksandr.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProjectService {
+    /**
+     * Transaction propagation
+     * REQUIRED (default)
+     * REQUIRES_NEW
+     * MANDATORY
+     * NEVER
+     * NOT_SUPPORTED
+     * NESTED
+     */
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void addTenProducts() {
+        for (int i = 0; i < 10; i++) {
+            productRepository.addProduct("Product " + i);
+        }
+    }
+}
